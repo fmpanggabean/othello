8b487296ae419cb7d4cfaf3b69006360d85666f9
F.M. Panggabean <cichio.maru@gmail.com>
2022-07-19 15:51:44 +0700

    tambah input controller

---
+++ Assets/Apps/Scripts/Othello/Gameplay/InputController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Othello.Gameplay
+{
+    public class InputController : MonoBehaviour
+    {
+        public event Action<BoardBlock> OnClickOnBoard;
+
+        public bool isEnabled { get; set; }
+
+        private void Update() {
+            if (!isEnabled) {
+                return;
+            }
+            if (Input.GetMouseButtonDown(0)) {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, 100)) {
+                    OnClickOnBoard?.Invoke(hit.collider.GetComponent<BoardBlock>());
+                }
+            }
+        }
+        public void Enable() {
+            isEnabled = true;
+        }
+        public void Disable() {
+            isEnabled = false;
+        }
+    }
+}
